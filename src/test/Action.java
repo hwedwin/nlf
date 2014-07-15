842ce227da578a27af23f3bb6518a9436c7bbbc9
6tail <6tail@6tail.cn>
2014-07-15 14:09:44 +0800

    后台增加文件上传处理

--- src/test/Action.java
+++ src/test/Action.java
@@ -5,11 +5,24 @@ import nc.liat6.frame.context.Statics;
 import nc.liat6.frame.db.transaction.ITrans;
 import nc.liat6.frame.db.transaction.TransFactory;
 import nc.liat6.frame.execute.Request;
+import nc.liat6.frame.execute.upload.UploadedFile;
 import nc.liat6.frame.paging.PageData;
+import nc.liat6.frame.web.WebExecute;
+import nc.liat6.frame.web.response.Json;
 import nc.liat6.frame.web.response.Paging;
+import nc.liat6.frame.web.upload.FileUploader;
 
-
+/**
+ * 示例
+ * @author 6tail
+ *
+ */
 public class Action{
+  
+  /**
+   * 自动分页
+   * @return
+   */
   public Object paging(){
     Request r = Context.get(Statics.REQUEST);
     ITrans t = TransFactory.getTrans();
@@ -18,5 +31,16 @@ public class Action{
     p.setPageData(pd);
     p.setUri("/demo/paging.jsp");
     return p;
+  }
+  
+  /**
+   * 文件上传
+   * @return
+   */
+  public Object upload(){
+    Request r = Context.get(Statics.REQUEST);
+    FileUploader uploader = r.find(WebExecute.TAG_UPLOADER);
+    UploadedFile file = uploader.getFile();
+    return new Json(file.getName());
   }
 }
