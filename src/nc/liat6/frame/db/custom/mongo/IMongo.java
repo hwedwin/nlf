e4bc70002386f0e3edb0f08fd2abe5372722369c
6tail <6tail@newtail>
2014-09-25 10:23:06 +0800

    删除框架中对mongodb的原生支持，需要时以插件形式单独引入

--- src/nc/liat6/frame/db/custom/mongo/IMongo.java
+++
@@ -1,9 +0,0 @@
-package nc.liat6.frame.db.custom.mongo;
-
-/**
- * MONGODB接口
- * 
- * @author 6tail
- * 
- */
-public interface IMongo{}
--- src/nc/liat6/frame/db/custom/mongo/MongoDriver.java
+++
@@ -1,3 +0,0 @@
-package nc.liat6.frame.db.custom.mongo;
-
-public class MongoDriver{}
