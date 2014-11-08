b61f2de87c6bfa6026aa8374823c3fda80eabb0f
6tail <6tail@newtail>
2014-11-08 22:31:10 +0800

    优化BeanPool

--- src/nc/liat6/frame/klass/BeanPool.java
+++ src/nc/liat6/frame/klass/BeanPool.java
@@ -5,40 +5,23 @@ import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.util.HashMap;
 import java.util.Map;
-import nc.liat6.frame.exception.NlfException;
 
 public class BeanPool{
 
   /** 缓存 */
-  private static Map<Class<?>,Map<Class<?>,BeanInfo>> STOP_INFO_MAP = new HashMap<Class<?>,Map<Class<?>,BeanInfo>>();
+  private static Map<Class<?>,BeanInfo> pool = new HashMap<Class<?>,BeanInfo>();
 
   private BeanPool(){}
 
   public static BeanInfo getBeanInfo(Class<?> klass){
-    try{
-      return Introspector.getBeanInfo(klass,Object.class);
-    }catch(IntrospectionException e){
-      throw new NlfException(e);
-    }
-  }
-
-  public static BeanInfo getBeanInfo(Class<?> klass,Class<?> stopClass){
-    if(Object.class==stopClass){
-      return getBeanInfo(klass);
-    }
-    Map<Class<?>,BeanInfo> map = STOP_INFO_MAP.get(klass);
-    if(null==map){
-      map = new HashMap<Class<?>,BeanInfo>();
-      STOP_INFO_MAP.put(klass,map);
-    }
-    BeanInfo info = map.get(stopClass);
+    BeanInfo info = pool.get(klass);
     if(null==info){
       try{
-        info = Introspector.getBeanInfo(klass,stopClass);
+        info = Introspector.getBeanInfo(klass,Object.class);
       }catch(IntrospectionException e){
-        throw new NlfException(e);
+        throw new RuntimeException(e);
       }
-      map.put(stopClass,info);
+      pool.put(klass,info);
     }
     return info;
   }
