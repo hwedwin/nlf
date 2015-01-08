95c556e8bf1f2003874aaa47ddb69f882c062cef
6tail <6tail@newtail>
2015-01-08 08:49:28 +0800

    去掉接口的public修饰符

--- src/nc/liat6/frame/db/dao/IDaoRule.java
+++ src/nc/liat6/frame/db/dao/IDaoRule.java
@@ -7,20 +7,19 @@ import nc.liat6.frame.db.transaction.ITrans;
 import nc.liat6.frame.paging.PageData;
 
 public interface IDaoRule{
-
   /**
    * 获取数据源别名
    *
    * @return 数据源别名
    */
-  public String alias();
+  String alias();
 
   /**
    * 获取Bean转换规则接口
    *
    * @return Bean转换规则接口
    */
-  public IBeanRule rule();
+  IBeanRule rule();
 
   /**
    * 查询一条记录
@@ -28,7 +27,7 @@ public interface IDaoRule{
    * @param t 事务接口
    * @return Bean
    */
-  public Bean one(ITrans t);
+  Bean one(ITrans t);
 
   /**
    * 查询列表
@@ -36,16 +35,15 @@ public interface IDaoRule{
    * @param t 事务接口
    * @return 列表
    */
-  public List<Bean> list(ITrans t);
+  List<Bean> list(ITrans t);
 
-  public void insert(ITrans t,Bean bean);
+  void insert(ITrans t,Bean bean);
 
-  public void delete(ITrans t);
+  void delete(ITrans t);
 
-  public int count(ITrans t);
+  int count(ITrans t);
 
-  public void update(ITrans t,Bean bean);
+  void update(ITrans t,Bean bean);
 
-  public PageData page(ITrans t);
-
+  PageData page(ITrans t);
 }
