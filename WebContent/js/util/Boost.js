c8048b4a78285e0f95cc0aa079b2c772ac5c67e5
6tail <6tail@newtail>
2015-02-14 22:35:23 +0800

    UI优化

--- WebContent/js/util/Boost.js
+++ WebContent/js/util/Boost.js
@@ -1,46 +1,33 @@
-/**
- * I.util.Boost
- * <i>增强功能</i>
- */
 I.regist('util.Boost',function(W,D){
- var _toCamelCase=function(){
-  if(s.indexOf('-')<0&&s.indexOf('_')<0){
-   return s;
-  }
-  return s.replace(/[-_][^-_]/g,function(m){
-   return m.charAt(1).toUpperCase();
-  });
- };
- var _addStyle=function(dom,style){
-  var st=dom.style.cssText;
-  if(';'!=st.substr(st.length-1)){
-   st+=';';
-  }
-  st+=style;
-  I.css(dom,st);
- };
- var _removeStyle=function(dom,styleName){
-  if(dom.style.removeProperty){
-   dom.style.removeProperty(styleName);
-  }else if(dom.style.removeAttribute){
-   dom.style.removeAttribute(_toCamelCase(styleName));
-  }
- };
- var _round=function(dom,n){
-  n=n+'';
-  var dw='px';
-  if(!/^[0-9]*$/.test(n.substr(n.length-1))){
-   dw='';
-  }
-  _addStyle(dom,'-webkit-border-radius:'+n+dw+';-moz-border-radius:'+n+dw+';-ms-border-radius:'+n+dw+';-o-border-radius:'+n+dw+';border-radius:'+n+dw+';');
- };
- return{
-  //转换为驼峰
-  toCamelCase:function(s){return _toCamelCase(s);},
-  //动态添加style代码
-  addStyle:function(dom,style){_addStyle(dom,style);},
-  //移除指定样式，样式名为非驼峰
-  removeStyle:function(dom,styleName){_removeStyle(dom,styleName);},
-  round:function(dom,n){_round(dom,n);}
- };
+var _toCamelCase=function(){
+ if(s.indexOf('-')<0&&s.indexOf('_')<0) return s;
+ return s.replace(/[-_][^-_]/g,function(m){
+  return m.charAt(1).toUpperCase();
+ });
+};
+var _addStyle=function(dom,style){
+ var st=dom.style.cssText;
+ if(';'!=st.substr(st.length-1)) st+=';';
+ st+=style;
+ I.css(dom,st);
+};
+var _removeStyle=function(dom,styleName){
+ if(dom.style.removeProperty) dom.style.removeProperty(styleName);
+ else if(dom.style.removeAttribute) dom.style.removeAttribute(_toCamelCase(styleName));
+};
+var _round=function(dom,n){
+ n=n+'';
+ var dw='px';
+ if(!/^[0-9]*$/.test(n.substr(n.length-1))) dw='';
+ _addStyle(dom,'-webkit-border-radius:'+n+dw+';-moz-border-radius:'+n+dw+';-ms-border-radius:'+n+dw+';-o-border-radius:'+n+dw+';border-radius:'+n+dw+';');
+};
+return {
+ //转换为驼峰
+ toCamelCase:function(s){return _toCamelCase(s);},
+ //动态添加style代码
+ addStyle:function(dom,style){_addStyle(dom,style);},
+ //移除指定样式，样式名为非驼峰
+ removeStyle:function(dom,styleName){_removeStyle(dom,styleName);},
+ round:function(dom,n){_round(dom,n);}
+};
 }+'');
--- WebContent/js/util/Hide.js
+++ WebContent/js/util/Hide.js
@@ -1,50 +1,25 @@
-/**
- * I.util.Hide
- * <i>隐藏显示字符串</i>
- * <u>I.util.Hide.show(s);</u>
- * <u>I.util.Hide.hide(s);</u>
- */
 I.regist('util.Hide',function(W,D){
-  var _show = function(s){
-    if(s.indexOf('\u200d')<0&&s.indexOf('\u200c')<0){
-      return s;
-    }
-    var n = [];
-    var ns = s.split('');
-    for(var i=0;i<ns.length;i++){
-      if(ns[i]=='\u200d'){
-        n.push('1');
-      }else if(ns[i]=='\u200c'){
-        n.push('0');
-      }
-    }
-    var rs = [];
-    var sb = [];
-    for(var i=0;i<n.length;i++){
-      if(sb.length<16){
-        sb.push(n[i]);
-      }else{
-        rs.push(String.fromCharCode(parseInt(sb.join(''),2)));
-        sb = [];
-        sb.push(n[i]);
-      }
-    }
-    if(sb.length>=16){
-      rs.push(String.fromCharCode(parseInt(sb.join(''),2)));
-    }
-    return rs.join('');
-  };
-    
-  var _hide = function(s){
-    throw 'util.Hide.hide(s) not implemented';
-  };
-    
-  return{
-    show:function(s) {
-      return _show(s);
-    },
-    hide:function(s) {
-      return _hide(s);
-    }
-  };
+var _show=function(s){
+ if(s.indexOf('\u200d')<0&&s.indexOf('\u200c')<0) return s;
+ var n=[],rs=[],sb=[],ns=s.split('');
+ for(var i=0,j=ns.length;i<j;i++){
+  if(ns[i]=='\u200d') n.push('1');
+  else if(ns[i]=='\u200c') n.push('0');
+ }
+ for(var i=0,j=n.length;i<j;i++){
+  if(sb.length<16) sb.push(n[i]);
+  else{
+   rs.push(String.fromCharCode(parseInt(sb.join(''),2)));
+   sb=[];
+   sb.push(n[i]);
+  }
+ }
+ if(sb.length>=16) rs.push(String.fromCharCode(parseInt(sb.join(''),2)));
+ return rs.join('');
+};
+var _hide=function(s){throw 'util.Hide.hide(s) not implemented';};
+return {
+ show:function(s){return _show(s);},
+ hide:function(s){return _hide(s);}
+};
 }+'');
