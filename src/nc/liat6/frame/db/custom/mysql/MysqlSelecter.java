7514b20852131ed1aa13b6aa8d3a6cb82ca244f4
6tail <6tail@newtail>
2015-03-09 14:44:32 +0800

    mysql增加is null条件的支持

---
+++ src/nc/liat6/frame/db/custom/mysql/MysqlSelecter.java
@@ -0,0 +1,11 @@
+package nc.liat6.frame.db.custom.mysql;
+
+import nc.liat6.frame.db.custom.oracle.OracleSelecter;
+
+/**
+ * mysql数据库查询器
+ * 
+ * @author 6tail
+ * 
+ */
+public class MysqlSelecter extends OracleSelecter implements IMysql{}
---
+++ src/nc/liat6/frame/db/custom/mysql/MysqlUpdater.java
@@ -0,0 +1,11 @@
+package nc.liat6.frame.db.custom.mysql;
+
+import nc.liat6.frame.db.custom.oracle.OracleUpdater;
+
+/**
+ * mysql数据库执行器
+ * 
+ * @author 6tail
+ * 
+ */
+public class MysqlUpdater extends OracleUpdater implements IMysql{}
